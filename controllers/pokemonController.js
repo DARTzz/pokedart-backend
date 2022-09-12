9b1a184e73e14f0679fe48f1ed1686f83ac21bc3
DARTzz <76001373+DARTzz@users.noreply.github.com>
2022-09-12 19:32:57 +0530

    fixed capital letter bug

--- controllers/pokemonController.js
+++ controllers/pokemonController.js
@@ -3,6 +3,13 @@ const Pokemon = require("./../models/pokemonModel.js");
 exports.getPokemonByID = async (req, res) => {
     try {
         const pokemon = await Pokemon.findOne({ id: req.params.id });
+        if (pokemon == null) {
+            res.status(404).json({
+                status: "fail",
+                message:
+                    "Could not find the Pokemon with the passed ID. Make sure the ID is correct !",
+            });
+        }
         res.status(200).json({
             status: "success",
             data: {
@@ -19,18 +26,28 @@ exports.getPokemonByID = async (req, res) => {
 };
 exports.getPokemonByName = async (req, res) => {
     try {
-        const pokemon = await Pokemon.findOne({ name: req.params.name });
+        const pokemon = await Pokemon.findOne({
+            name:
+                req.params.name[0].capitalize() + req.params.name.substring(1),
+        });
+        if (pokemon == null) {
+            res.status(404).json({
+                status: "fail",
+                message:
+                    "Could not find the Pokemon with the passed Name. Make sure the name is correct !",
+            });
+        }
         res.status(200).json({
             status: "success",
             data: {
-                pokemon,
+                pokemon: { ...pokemon },
             },
         });
     } catch (err) {
         res.status(404).json({
             status: "fail",
             message:
-                "Could not find the Pokemon with the passed id. Make sure the id is correct !",
+                "Could not find the Pokemon with the passed Name. Make sure the name is correct !",
         });
     }
 };
